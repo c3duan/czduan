a42140172fae79cedad90f99adf4c94e069e1826
Chengzhu Duan <Chengzhu.Duan@gmail.com>
2020-03-24 18:00:20 -0700

    Updates

---
+++ precache-manifest.a1aa90a01c8e1d77cff49b4b51cf6bfd.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "51a28e250b835993e64b",
+    "url": "/czduan/static/css/404.e80df8f7.chunk.css"
+  },
+  {
+    "revision": "51a28e250b835993e64b",
+    "url": "/czduan/static/js/404.51a28e25.chunk.js"
+  },
+  {
+    "revision": "6a26482b8a73870d3883",
+    "url": "/czduan/static/css/About.629bcc50.chunk.css"
+  },
+  {
+    "revision": "6a26482b8a73870d3883",
+    "url": "/czduan/static/js/About.6a26482b.chunk.js"
+  },
+  {
+    "revision": "ded1ac129eaf864dbaf1",
+    "url": "/czduan/static/css/Contact.2feef356.chunk.css"
+  },
+  {
+    "revision": "ded1ac129eaf864dbaf1",
+    "url": "/czduan/static/js/Contact.ded1ac12.chunk.js"
+  },
+  {
+    "revision": "f47274ced6e61d671016",
+    "url": "/czduan/static/css/Home.00a94a17.chunk.css"
+  },
+  {
+    "revision": "f47274ced6e61d671016",
+    "url": "/czduan/static/js/Home.f47274ce.chunk.js"
+  },
+  {
+    "revision": "d4cefc7e602ef7c16ba9",
+    "url": "/czduan/static/css/Work.9466c591.chunk.css"
+  },
+  {
+    "revision": "d4cefc7e602ef7c16ba9",
+    "url": "/czduan/static/js/Work.d4cefc7e.chunk.js"
+  },
+  {
+    "revision": "a1394cc3bb8be9e0d6b1",
+    "url": "/czduan/static/css/main.c1aa0ae4.chunk.css"
+  },
+  {
+    "revision": "a1394cc3bb8be9e0d6b1",
+    "url": "/czduan/static/js/main.a1394cc3.chunk.js"
+  },
+  {
+    "revision": "e00126f3fb0638762247",
+    "url": "/czduan/static/js/runtime~main.e00126f3.js"
+  },
+  {
+    "revision": "23e4e7ebc319c35bb5ff",
+    "url": "/czduan/static/js/7.23e4e7eb.chunk.js"
+  },
+  {
+    "revision": "4e016688f76bbb7e01b4",
+    "url": "/czduan/static/js/8.4e016688.chunk.js"
+  },
+  {
+    "revision": "9466a912e13e974a21fe3fff1beff6fa",
+    "url": "/czduan/static/media/logo--no-back.9466a912.png"
+  },
+  {
+    "revision": "06d466b25005c496fb0ba3f8518f6240",
+    "url": "/czduan/static/media/back.06d466b2.webp"
+  },
+  {
+    "revision": "89c5e4e8de88bd74683194983500b175",
+    "url": "/czduan/static/media/back.89c5e4e8.jpeg"
+  },
+  {
+    "revision": "76e78aa051f7bc355273e22646dda364",
+    "url": "/czduan/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e167a830a5fcac79cbbf57752c1d5fc3.js"
+  "/czduan/precache-manifest.a1aa90a01c8e1d77cff49b4b51cf6bfd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/czduan/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
